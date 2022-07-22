05f1e3bb0b30224117f40837d291cdca417c0f20
Yauris12 <alexisaube28@gmail.com>
2022-07-22 17:47:49 -0500

    first commit

---
+++ models/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const path = require('path')
+const Sockets = require('./sockets')
+class Server {
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT
+
+    //HTTP SERVER
+    this.server = http.createServer(this.app)
+    //Configuration sockets
+
+    //config del socket server
+    this.io = socketio(this.server, {
+      // configuracions
+    })
+  }
+
+  middlewares() {
+    //directorio public
+    this.app.use(express.static(path.resolve(__dirname, '../public')))
+  }
+  configuracionSockets() {
+    new Sockets(this.io)
+  }
+  execute() {
+    this.middlewares()
+
+    //inicializar socketio
+    this.configuracionSockets()
+
+    this.server.listen(this.port, () => {
+      console.log('server corriendo en puerto', this.port)
+    })
+  }
+}
+
+module.exports = Server
